b124b3bbd98b23b197ef709744fdd15e9fcbe451
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-11 12:11:51 +0200

    dodane dalsze wymagania do proj

--- src/com/krotos/Main.java
+++ src/com/krotos/Main.java
@@ -18,4 +18,14 @@ class Main {
 
     //get
     //add
+    //dodac cykl zawodów i obliczanie pkt w calym cyklu
+    //dla 6 zawodów bierze sie najlepsze wyniki z 4 (dla każdego) (dwa odpadają - zawsze 2)
+    //pierwszych x osob kwalifikuje się do fianału
+    //i od nowa
+    //jak kogos nie było na zawodach to dostaje tam max ilość pkt sektorowych(z nawiekszego sektora) +1
+    //dodac eksport do plików
+    //jedna wspolna baze
+
+    //trello do robienia planow projektów
+
 }
