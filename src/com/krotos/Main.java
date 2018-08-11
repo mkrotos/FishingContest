bfe0e2423a94ea8605a4f94ee2dcfa182bf678e0
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-11 12:22:15 +0200

    dalsze wymagania do person

--- src/com/krotos/Main.java
+++ src/com/krotos/Main.java
@@ -13,7 +13,6 @@ class Main {
         Map<String,Tournament> mapOfTournaments=new HashMap<>();
 
 
-
     }
 
     //get
--- src/com/krotos/Person.java
+++ src/com/krotos/Person.java
@@ -4,11 +4,13 @@ public class Person {
 
     String name;
     String surname;
+    int age;
+    char sex;
 
     //get
     //set
     //toString
-
+    //dodac grupy w klasyfikacji wg wieku i plci (automatyczne dodawanie) + seter gdyby ktos chciał potem zmienic
 
 }
 
