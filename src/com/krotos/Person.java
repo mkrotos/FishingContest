dfde41f16d8f8f2dc63d429884b8d4114f9fd5c6
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-31 21:05:29 +0200

    person correct

--- src/com/krotos/Person.java
+++ src/com/krotos/Person.java
@@ -2,14 +2,69 @@ package com.krotos;
 
 public class Person {
 
-    String name;
-    String surname;
-    int age;
-    char sex;
+    private String name;
+    private String surname;
+    private int age;
+    private char sex;
 
-    //get
-    //set
-    //toString
+    public Person() {
+    }
+
+    public Person(String name, String surname, int age, char sex) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    @Override
+    public String toString() {
+        return  name+" "+surname+", wiek: "+age+", płeć:"+sex;
+    }
+
     //dodac grupy w klasyfikacji wg wieku i plci (automatyczne dodawanie) + seter gdyby ktos chciał potem zmienic
 
 }
--- src/com/krotos/Tournament.java
+++ src/com/krotos/Tournament.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Tournament {
-    Random random=new Random();
+    private Random random=new Random();
 
     private String tournamentName;
     private Date dateOfTournament;
