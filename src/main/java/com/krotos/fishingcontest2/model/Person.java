dcd25d2cf60978f9e55273627efdc9ca1425052c
Mariusz Krotos <mar.krotos@gmail.com>
2018-09-25 16:06:01 +0200

    lombok

--- src/main/java/com/krotos/fishingcontest2/model/Person.java
+++ src/main/java/com/krotos/fishingcontest2/model/Person.java
@@ -1,9 +1,12 @@
 package com.krotos.fishingcontest2.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class Person {
 
@@ -17,59 +20,4 @@ public class Person {
     private String phoneNumber;
     private String mail;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Character getSex() {
-        return sex;
-    }
-
-    public void setSex(Character sex) {
-        this.sex = sex;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
 }
